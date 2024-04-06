8317b9a1c0b5a0067e2c30254c3af7b3d24edeaa
s00ny5 <30322461+s00ny5@users.noreply.github.com>
2024-04-06 15:09:05 +0300

    Create farm.js

---
+++ farm.js
@@ -0,0 +1,8 @@
+// Import cox-chain dependency
+const coxChain = require('cox-chain');
+
+// Log Web3 version
+console.log('Web3 version:', coxChain.Web3.version);
+
+// Use cox-chain dependency
+console.log('cox-chain version:', coxChain.version);
